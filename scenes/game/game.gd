76120aa54d3d33b300d6f0d9d315966b595acb35
karjon7 <80655086+karjon7@users.noreply.github.com>
2024-07-16 01:51:23 -0400

    Update game.gd

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -1,9 +1,21 @@
 extends Node
 
+#Difficulty
 enum DIFFICULTIES {
 	EASY,
 	NORMAL,
 	HARD,
 }
-
 var current_difficulty : DIFFICULTIES
+
+#Character
+enum CHARACTERS {
+	JOHN,
+	EMILY,
+}
+var playing_as : CHARACTERS
+
+
+func _init(_playing_as : CHARACTERS = CHARACTERS.JOHN, _difficulty : DIFFICULTIES = DIFFICULTIES.NORMAL):
+	playing_as = _playing_as
+	current_difficulty = _difficulty
